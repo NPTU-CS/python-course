1fde565bb94159d7a6178d12220aadf89af2d6a7
mysper <45904933+mysper@users.noreply.github.com>
2021-04-09 01:42:15 +0800

    Create 變數運算.py

---
+++ 變數/變數運算.py
@@ -0,0 +1,24 @@
+# 加減乘除
+a = 1 + 2
+b = 3 - 4
+c = 5 * 6
+d = 7 / 8
+
+# 次方
+e = 9 ** 10
+
+# 取餘數
+f = 12 % 5
+
+# 字串運算
+g = 'Hello' + ' ' + 'World'
+
+# -----
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
