a6a1599a58b8cac7a6b8e57017d19d03e378ce8a
mysper <45904933+mysper@users.noreply.github.com>
2021-04-09 01:44:44 +0800

    Create BMI.py

---
+++ 變數/BMI.py
@@ -0,0 +1,9 @@
+weight = input('Please enter your weight: ')
+height = input('Please enter your height: ')
+
+weight = float(weight)
+height = float(height)
+
+bmi = weight / (height**2)
+
+print(bmi)
